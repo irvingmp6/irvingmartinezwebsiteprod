4559e6d9364f80b12f5cd79b4d873825911f7ce5
Irving Martinez <irvingmp6@hotmail.com>
2023-09-08 21:26:23 -0500

    added pictures and updated colors scheme

--- irvingmartinez/front_page/urls.py
+++ irvingmartinez/front_page/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='front_page-home')
+    path('', views.home, name='front_page-home'),
+    path('admin/', views.dont_do_that, name='front_page-dont-do-that')
 ]
