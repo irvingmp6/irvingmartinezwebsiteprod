bc509988da01243f15fbf01be5cd92e4d5d6b192
Irving Martinez <irvingmp6@hotmail.com>
2023-09-08 21:27:06 -0500

    merged 20230908-001

--- irvingmartinez/front_page/views.py
+++ irvingmartinez/front_page/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'front_page/home.html')
+
+def dont_do_that(request):
+    return render(request, 'front_page/dont_do_that.html')
--- irvingmartinez/irvingmartinez/urls.py
+++ irvingmartinez/irvingmartinez/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('back-door-entrance-gaurded-by-hired-bouncers/', admin.site.urls),
+    # path('back-door-entrance-gaurded-by-hired-bouncers/', admin.site.urls),
     path('', include('front_page.urls'))
 ]
